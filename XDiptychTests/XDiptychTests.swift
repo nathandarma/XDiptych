0eea0f9b45392276e8bd62c07c604526d522dee6
nathandarma <nathandarma730@gmail.com>
2025-06-07 09:02:47 +1000

    Initial Commit

---
+++ XDiptychTests/XDiptychTests.swift
@@ -0,0 +1,17 @@
+//
+//  XDiptychTests.swift
+//  XDiptychTests
+//
+//  Created by Nathan Darma on 7/6/2025.
+//
+
+import Testing
+@testable import XDiptych
+
+struct XDiptychTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
